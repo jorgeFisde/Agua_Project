82fe74d7ebfb7b6ef867470a64a2f7feaa6b79f4
jorge fischer <jorgefisde@gmail.com>
2019-10-04 08:55:07 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.post('/', (req, res, next) => {
+    // Aqui es el POST, donde resivirre tu fakin JSON a este link: http://localhost/
+    res.send('Todo bien, todo correcto, y yo que me alegro')
+})
+app.get('/', (req, res) => {
+    // Esto es lo que te enviare si me haces una peticion GET gei
+    res.send('enviame JSON hijo de perra')
+})
+
+app.listen('3000', () => {
+    console.log('Servidor en puerto: 3000');
+})
