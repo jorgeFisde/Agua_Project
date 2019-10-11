e92fe741fa73e773ededc4352c112a9c39760dc4
jorge fischer <jorgefisde@gmail.com>
2019-10-11 12:08:18 -0600

    commit bda

--- index.js
+++ index.js
@@ -1,19 +1,81 @@
 const express = require('express')
+const mysql = require('mysql')
 const app = express()
+
+const connection = mysql.createConnection(
+    {
+        host: "localhost",
+        user: "root",
+        password: "toor",
+        database: "project_a"
+    }
+)
+
+connection.connect((err)=>{
+    if (err){
+        console.log("hubo un error:", err);
+        return
+    }else{
+        console.log("todo bien todo correcto");
+        
+    }
+})
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.post('/', (req, res, next) => {
-    // Aqui es el POST, donde resivirre tu fakin JSON a este link: http://localhost/
+app.get('/', (req, res, next) => {
+    // Aqui es el GET, donde resivirre tu fakin JSON a este link: http://localhost/
     res.send("puto")
-    console.log('alguien me hizi post');
-    
+    console.log('alguien me hizi GET');
+
 })
-app.get('/', (req, res) => {
+app.post('/registrarse', (req, res) => {
     // Esto es lo que te enviare si me haces una peticion GET gei!
-    res.send('enviame JSON hijo de perra')
+    const echo = req.body
+    const sql = `INSERT INTO usuario (nombre,apellido,fecha_nacimiento,correo, contraseña) VALUES (?,?,?,?,?)`
+    connection.query(sql,[echo.nombre,echo.apellido,echo.fecha_nacimiento,echo.correo,echo.contraseña],(err, data)=>{
+        if (err) {
+            console.log(err.sqlMessage);
+            
+        }else{
+            res.send(data)
+        }
+    })
 })
+app.post('/crearTinaco', (req, res) => {
+    // Esto es lo que te enviare si me haces una peticion GET gei!
+    const echo = req.body
+    const sql = `
+    INSERT INTO tinaco (id,nombre)
+    VALUES (?,?)
+    `
+    connection.query(sql,[echo.id,echo.nombre],(err, result)=>{
+        if (err) {
+            console.log(err.sqlMessage);
+            
+        }else{
+            res.json(result)
+        }
+    })
+})
+app.post('/sendDatos', (req, res) => {
+    // Esto es lo que te enviare si me haces una peticion GET gei!
+    const echo = req.body
+    const sql = `
+    INSERT INTO datos (porcentaje,id_tinaco ,id_usuario ,)
+    VALUES (?,?,?)
+    `
+    connection.query(sql,[echo.porcentaje,echo.id_tinaco, echo.id_usuario],(err, result)=>{
+        if (err) {
+            console.log(err.sqlMessage);
+            
+        }else{
+            res.send(result)
+        }
+    })
+})
+
 
 app.listen('3000', () => {
     console.log('Servidor en puerto: 3000');
