3bfbcff0b02067a0aa8f850cd20c3c6eba6753cd
jorge fischer <jorgefisde@gmail.com>
2019-10-18 15:54:41 -0600

    rew

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use('/api',require('./routes/login').router)
 app.use('/api',require('./routes/register'))
 app.use('/api',require('./routes/Data'))
 app.use('/api',require('./routes/tinaco'))
+app.use('/api',require('./routes/index'))
 
 app.listen('3000', () => {
     console.log('Servidor en puerto: 3000');
--- routes/index.js
+++ routes/index.js
@@ -27,3 +27,4 @@ router.get('/home',user.verificacionToken,(req,res,next)=>{
         }
     })
 })
+module.exports = router
