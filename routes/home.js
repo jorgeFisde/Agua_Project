e8400954ff5dac03e105975b04c2ee165c81c3ae
jorgeFisde <54189297+jorgeFisde@users.noreply.github.com>
2019-11-04 16:18:56 -0700

    new request commit mac

---
+++ routes/home.js
@@ -0,0 +1,47 @@
+ const router  = require("express").Router()
+ const jwt = require('jsonwebtoken')
+
+ const connection = require('../service/dataBase')
+ const user = require('./login')
+
+ router.get("/home",user.verificacionToken,(req,res,next)=>{
+    jwt.verify(req.token,'my_secret_key',(err,data)=>{
+        if (err) {
+            console.log('error al obtener el token: ') + err;
+            res.send('Ups! hubor un problema en la autenticacion')
+        }else{
+            var sql = 'SELECT id_tinaco,nombre FROM datos,tinaco WHERE datos.id_tinaco = tinaco.id AND datos.id_usuario = ?'
+            connection.query(sql,[data.usuario.id],(err,rows)=>{
+                if (err) {
+                    console.log(err.sqlMessage);
+                    res.send('hubo un error a consultar la base de datos')
+                }else{
+                    res.json(rows)
+                    console.log(rows);
+                    
+                }
+            })
+        }
+    })
+ })
+
+ router.get("/home/porcentaje",user.verificacionToken,(req,res,next)=>{
+    jwt.verify(req.token,'my_secret_key',(err,data)=>{
+        if (err) {
+            console.log('error al obtener el token: ') + err;
+            res.send('Ups! hubor un problema en la autenticacion')
+        }else{
+            var sql = 'SELECT porcentaje FROM datos WHERE id_usuario = ?'
+            connection.query(sql,[data.usuario.id],(err,rows)=>{
+                if (err) {
+                    console.log(err.sqlMessage);
+                    res.send('hubo un error a consultar la base de datos')
+                }else{
+                    res.json(rows)
+                    console.log(rows);
+                    
+                }
+            })
+        }
+    })
+ })
